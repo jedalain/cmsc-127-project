8fa1af34b4fb641b51d5ef80efe4c6f847c63237
Jed Alain <110464712+jedalain@users.noreply.github.com>
2024-06-03 09:22:48 +0800

    delete: removed backend directory

--- backend/src/routes/router.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from 'express';
-import { getAllEstablishments } from '../controllers/foodEstablishmentController';
-
-
-function router(app: Express) {
-    app.get("/", getAllEstablishments)
-  }
-  
-  export default router;
